fbfdb0616e5c2f5b75b32b85f1ad1bdb055a7bee
Gabriel Setznagl <91721431+Setznagl@users.noreply.github.com>
2024-06-02 18:54:51 -0300

    Starting the resolution of the exercise number three

--- src/Exercicios_29_05/ex_01/Main.java
+++ src/Exercicios_29_05/ex_01/Main.java
@@ -1,34 +1,13 @@
 package Exercicios_29_05.ex_01;
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
     static Scanner LerDados = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.println("""
-                Bem vindo ao sistema de cadastro de livros
-                O que deseja realizar?\s
-                1- Cadastrar Livros
-                2- Listar Livros
-                """);
 
-        int Selecionado = LerOpcao();
-        System.out.println("Foi selecionado a opção " + Selecionado);
+        fluxoDeEscolha();
 
-        if (Selecionado == 1){
-            System.out.println("Digite nome, autor, editora, ano de publicação e preço");
-                Livro livro1 = new Livro();
-                Biblioteca.AdicionarLivro(livro1);
-            System.out.println("Digite nome, autor, editora, ano de publicação e preço");
-                Livro livro2 = new Livro();
-                Biblioteca.AdicionarLivro(livro2);
-            System.out.println("Digite nome, autor, editora, ano de publicação e preço");
-                Livro livro3 = new Livro();
-                Biblioteca.AdicionarLivro(livro3);
-
-            Biblioteca.exibirLivros();
-        }
     }
 
     static int LerOpcao() {
@@ -41,11 +20,54 @@ public class Main {
                         O que deseja realizar?\s
                         1- Cadastrar Livros
                         2- Listar Livros
+                        3- Finalizar
                         """);
                 Opcao = LerDados.nextInt();
                 System.out.println("Você selecionou " + Opcao + ", Deseja alterar? \n" + "1 - Alterar\n" + "2 - Manter");
                 Confirmacao = LerDados.nextInt();
             }
         return Opcao;
+    }
+
+    static void fluxoDeEscolha(){
+        System.out.println("""
+                Bem vindo ao sistema de cadastro de livros
+                O que deseja realizar?\s
+                1- Cadastrar Livros
+                2- Listar Livros
+                3- Finalizar Programa
+                """);
+
+        int Selecionado = LerOpcao();
+        System.out.println("Foi selecionado a opção " + Selecionado);
+
+        switch (Selecionado){
+            case 1:{
+                System.out.println("Digite nome, autor, editora, ano de publicação e preço");
+                Livro livro1 = new Livro();
+                Biblioteca.AdicionarLivro(livro1);
+                System.out.println("Digite nome, autor, editora, ano de publicação e preço");
+                Livro livro2 = new Livro();
+                Biblioteca.AdicionarLivro(livro2);
+                System.out.println("Digite nome, autor, editora, ano de publicação e preço");
+                Livro livro3 = new Livro();
+                Biblioteca.AdicionarLivro(livro3);
+
+                System.out.println("Livros cadastrados com sucesso");
+
+                System.out.println("Deseja realizar outra função?\n 1- sim     2- não ");
+                    int AcaoAdicional = LerOpcao();
+                    if (AcaoAdicional == 1){
+                        fluxoDeEscolha();
+                        break;
+                    }
+                break;
+            }
+            case 2:{
+                Biblioteca.exibirLivros();
+                break;
+            }
+            default: break;
+        }
     }
 }
---
+++ src/Exercicios_29_05/ex_02/Aluno.java
@@ -0,0 +1,25 @@
+package Exercicios_29_05.ex_02;
+import java.util.Scanner;
+
+public class Aluno {
+    private String NomeAluno;
+    private int IdadeAluno;
+    private int MatriculaAluno;
+
+    Scanner LerDados = new Scanner(System.in);
+
+    protected Aluno(){
+        this.NomeAluno = LerDados.nextLine();
+        this.IdadeAluno = LerDados.nextInt();
+        this.MatriculaAluno = LerDados.nextInt();
+    }
+
+    @Override
+    public String toString(){
+        return "Registro de Aluno\n"+
+                "Nome :" + "  " + NomeAluno + "  " +
+                "Idade :" + "  " + IdadeAluno + "  " +
+                "Matrícula :" + "  " + MatriculaAluno + "  " + "\n";
+    }
+
+}
