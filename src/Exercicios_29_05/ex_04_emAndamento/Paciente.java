ff4721eeff63610f208af123d1b47ebc7285a1c8
Gabriel Setznagl <gabrielembraer3@gmail.com>
2024-06-03 18:00:11 -0300

    Starting the exercise number 4

--- src/Exercicios_29_05/ex_04_emAndamento/Paciente.java
+++ src/Exercicios_29_05/ex_04_emAndamento/Paciente.java
@@ -1,4 +1,23 @@
 package Exercicios_29_05.ex_04_emAndamento;
+import java.util.Scanner;
 
-protected class Paciente {
+public class Paciente {
+    static String NomePaciente;
+    static double AlturaPaciente;
+    static double PesoPaciente;
+
+    Scanner LerDados = new Scanner(System.in);
+
+    protected Paciente() {
+        this.NomePaciente = setNome();
+        this.AlturaPaciente = LerDados.nextDouble();
+        this.PesoPaciente = LerDados.nextDouble();
+    }
+
+    protected String setNome(){return LerDados.nextLine();}
+
+    @Override
+    public String toString(){
+        return  " "+ NomePaciente + " Altura: " + AlturaPaciente + " Peso: " + PesoPaciente;
+    }
 }
--- src/Exercicios_29_05/ex_04_emAndamento/Principal.java
+++ src/Exercicios_29_05/ex_04_emAndamento/Principal.java
@@ -1,8 +1,79 @@
 package Exercicios_29_05.ex_04_emAndamento;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Principal {
 
+    protected static Scanner LerDados = new Scanner(System.in);
+    protected static List<Paciente> ListaPacientes = new ArrayList<>();
+
     public static void main(String[] args) {
 
+        fluxoDeEscolha();
+
+    }
+
+    static int LerOpcao() {
+        int Opcao = LerDados.nextInt();
+        System.out.println("Você selecionou " + Opcao + ", Deseja alterar? \n" + "1 - Alterar\n" + "2 - Manter");
+        int Confirmacao = LerDados.nextInt();
+        while (Confirmacao == 1) {
+            System.out.println("""
+                    Bem vindo ao sistema de cadastro de pacientes para cálculo de IMC.
+                    O que deseja realizar?\s
+                    1- Cadastrar paciente 
+                    2- Calular IMC dos pacientes e exibir relatórios
+                    3- Finalizar
+                    """);
+            Opcao = LerDados.nextInt();
+            System.out.println("Você selecionou " + Opcao + ", Deseja alterar? \n" + "1 - Alterar\n" + "2 - Manter");
+            Confirmacao = LerDados.nextInt();
+        }
+        return Opcao;
+    }
+
+    static void AdicionarPaciente(Paciente pacienteIterado){
+        ListaPacientes.add(pacienteIterado);
+    }
+
+    static void fluxoDeEscolha() {
+        System.out.println("""
+                    Bem vindo ao sistema de cadastro de pacientes para cálculo de IMC.
+                    O que deseja realizar?\s
+                    1- Cadastrar paciente 
+                    2- Calular IMC dos pacientes e exibir relatórios
+                    3- Finalizar
+                    """);
+
+        int Selecionado = LerOpcao();
+        System.out.println("Foi selecionado a opção " + Selecionado);
+
+        switch (Selecionado) {
+            case 1: {
+                System.out.println("Digite um nome para o paciente ou deixe em branco e em" +
+                        " seguida digite sua altura e peso respectivamente");
+                // Adicionando produtos ao carrinho
+                Paciente paciente1 = new Paciente();
+                AdicionarPaciente(paciente1);
+                System.out.println("Cadastrado com sucesso!");
+
+                System.out.println("Pacientes cadastrados com sucesso");
+
+                System.out.println("Deseja realizar outra função?\n 1- sim     2- não ");
+                int AcaoAdicional = LerOpcao();
+                if (AcaoAdicional == 1) {
+                    fluxoDeEscolha();
+                    break;
+                }
+                break;
+            }
+            case 2: {
+                System.out.println("Método para saída");
+                break;
+            }
+            default:
+                break;
+        }
     }
 }
