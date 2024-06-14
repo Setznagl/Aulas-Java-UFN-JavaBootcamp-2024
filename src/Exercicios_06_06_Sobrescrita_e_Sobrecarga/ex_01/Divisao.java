43fd142c145e5826b01b88096cef0294b08da18f
Gabriel Setznagl <gabrielembraer3@gmail.com>
2024-06-14 02:00:45 -0300

    .
    .

--- src/Exercicios_06_06_Sobrescrita_e_Sobrecarga/ex_01/Divisao.java
+++ src/Exercicios_06_06_Sobrescrita_e_Sobrecarga/ex_01/Divisao.java
@@ -1,4 +1,4 @@
 package Exercicios_06_06_Sobrescrita_e_Sobrecarga.ex_01;
 
-abstract Subtracao extends Operacao{
+class Divisao extends Operacao{
 }
--- src/Exercicios_06_06_Sobrescrita_e_Sobrecarga/ex_01/Soma.java
+++ src/Exercicios_06_06_Sobrescrita_e_Sobrecarga/ex_01/Soma.java
@@ -1,6 +1,6 @@
 package Exercicios_06_06_Sobrescrita_e_Sobrecarga.ex_01;
 
-abstract Soma extends Operacao{
+class Soma extends Operacao{
     @Override
     public double calcular() {
         resultado = a + b;
