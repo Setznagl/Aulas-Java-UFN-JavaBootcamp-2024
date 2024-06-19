effe29c992347163b02f32b2ac69d20dfef15800
Gabriel Setznagl <gabrielembraer3@gmail.com>
2024-06-19 20:18:45 -0300

    .
    .

--- src/Exercicio_Final_Java/interfaces/ProdutoBase.java
+++ src/Exercicio_Final_Java/interfaces/ProdutoBase.java
@@ -1,38 +1,55 @@
 package Exercicio_Final_Java.interfaces;
 
-abstract class ProdutoBase implements Produto{
+public abstract class ProdutoBase implements Produto {
     private String nome;
     private String preco;
     private int qtd;
 
     @Override
-    public String getTipo(){
+    public String getTipo() {
         return "";
-    };
+    }
+
+    ;
 
     @Override
-    public String getNome(){
+    public String getNome() {
         return "";
-    };
+    }
+
+    ;
 
     @Override
-    public double getPreco(){
+    public double getPreco() {
         return 0;
-    };
+    }
+
+    ;
 
     @Override
-    public int getQuantidade(){
+    public int getQuantidade() {
         return 0;
-    };
+    }
+
+    ;
 
     @Override
-    public void setNome(String nome){};
+    public void setNome(String nome) {
+    }
+
+    ;
 
     @Override
-    public void setPreco(double preco){};
+    public void setPreco(double preco) {
+    }
+
+    ;
 
     @Override
-    public void setQtde(int qtde){};
+    public void setQtde(int qtde) {
+    }
+
+    ;
 
     @Override
     public abstract String exibirDetalhes();
